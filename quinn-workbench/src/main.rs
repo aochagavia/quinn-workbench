9655160e19fbada8de9efc900b1972dba7f1e273
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2024-05-28 13:14:52 +0200

    Add Marc's demo client-server application

--- quinn-workbench/src/main.rs
+++ quinn-workbench/src/main.rs
@@ -1,12 +1,16 @@
+mod no_cc;
+
+use crate::no_cc::NoCCConfig;
 use anyhow::{anyhow, Context};
 use in_memory_network::{InMemoryNetwork, PcapExporter, SERVER_ADDR};
 use quinn::crypto::rustls::QuicClientConfig;
 use quinn::rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use quinn::rustls::RootCertStore;
 use quinn::{ClientConfig, Endpoint, EndpointConfig, TransportConfig, VarInt};
+use quinn_proto::AckFrequencyConfig;
 use rustls::pki_types::PrivatePkcs8KeyDer;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 fn main() -> anyhow::Result<()> {
     std::env::set_var("SSLKEYLOGFILE", "keylog.key");
@@ -29,7 +33,7 @@ async fn run() -> anyhow::Result<()> {
 
     // Network
     let pcap_exporter = Arc::new(PcapExporter::new());
-    let simulated_link_delay = Duration::from_secs(0);
+    let simulated_link_delay = Duration::from_secs(5);
     let simulated_link_capacity = usize::MAX;
     let network = Arc::new(InMemoryNetwork::initialize(
         simulated_link_delay,
@@ -45,18 +49,25 @@ async fn run() -> anyhow::Result<()> {
     let client = client_endpoint(cert, network)?;
     let connection = client.connect(SERVER_ADDR, server_name)?.await?;
 
-    let mut received = Vec::new();
-    for b in 42..45 {
+    let request_number = 5;
+    let request = "GET /index.html";
+    let start = Instant::now();
+    for _ in 0..request_number {
+        println!("{:.02}s {request}", start.elapsed().as_secs_f64());
+
         let (mut tx, mut rx) = connection.open_bi().await?;
-        tx.write_all(&[b]).await?;
+        tx.write_all(request.as_bytes()).await?;
+        tx.finish()?;
 
-        let mut buf = [0];
-        rx.read_exact(&mut buf).await?;
-
-        received.push(buf[0]);
+        rx.read_to_end(usize::MAX).await?;
     }
 
-    connection.close(VarInt::from_u32(0), b"done");
+    println!(
+        "Done! Sent {request_number} requests in {:.02}s",
+        start.elapsed().as_secs_f64()
+    );
+
+    connection.close(VarInt::from_u32(0), &[]);
 
     drop(connection);
     drop(client);
@@ -65,8 +76,6 @@ async fn run() -> anyhow::Result<()> {
         .await
         .context("server task crashed")?
         .context("server task errored")?;
-
-    println!("Done! Received: {received:?}");
 
     pcap_exporter.save("capture.pcap".as_ref());
 
@@ -80,13 +89,19 @@ async fn server_listen(endpoint: Endpoint) -> anyhow::Result<()> {
         .ok_or(anyhow!("failed to accept incoming connection"))?
         .await?;
 
-    while let Ok((mut tx, mut rx)) = conn.accept_bi().await {
-        // Read a single byte
-        let mut buf = [0];
-        rx.read_exact(&mut buf).await?;
+    let response = "<html>
+      <h1>Hello from Internet in Deep Space</h1>
+      <p>This message was sent over HTTP/QUIC/UDP/IP with a long delay</p>
+    </html>";
 
-        // Echo it back
-        tx.write(&buf).await?;
+    while let Ok((mut tx, mut rx)) = conn.accept_bi().await {
+        // Read the request
+        let request = rx.read_to_end(usize::MAX).await?;
+        assert_eq!(request, b"GET /index.html");
+
+        // Respond
+        tx.write(response.as_bytes()).await?;
+        tx.finish()?;
     }
 
     Ok(())
@@ -152,7 +167,20 @@ fn client_config(server_cert: CertificateDer<'_>) -> anyhow::Result<ClientConfig
 fn transport_config() -> TransportConfig {
     let mut config = TransportConfig::default();
     config.mtu_discovery_config(None);
-    // config.initial_rtt(Duration::from_secs(5));
+
+    // DTN stuff
+    config.max_idle_timeout(Some(VarInt::MAX.into()));
+    config.receive_window(VarInt::MAX);
+    config.datagram_send_buffer_size(usize::MAX);
+    config.send_window(u64::MAX);
+    config.datagram_receive_buffer_size(Some(usize::MAX));
+    config.stream_receive_window(VarInt::MAX);
+    config.congestion_controller_factory(Arc::new(NoCCConfig::default()));
+    let mut ack_frequency_config = AckFrequencyConfig::default();
+    ack_frequency_config.max_ack_delay(Some(Duration::MAX));
+    config.ack_frequency_config(Some(ack_frequency_config));
+    config.packet_threshold(u32::MAX);
+    config.initial_rtt(Duration::from_secs(100000));
 
     config
 }
---
+++ quinn-workbench/src/no_cc.rs
@@ -0,0 +1,83 @@
+use quinn::congestion::{Controller, ControllerFactory};
+use quinn_proto::RttEstimator;
+use std::any::Any;
+use std::sync::Arc;
+use std::time::Instant;
+
+#[derive(Debug, Default, Clone)]
+
+/// No congestion control
+pub struct NoCC {
+    config: Arc<NoCCConfig>,
+    /// Maximum number of bytes in flight that may be sent.
+    window: u64,
+}
+
+impl NoCC {
+    pub fn new(config: Arc<NoCCConfig>, _now: Instant, _current_mtu: u16) -> Self {
+        Self {
+            window: config.initial_window,
+            config,
+        }
+    }
+}
+
+impl Controller for NoCC {
+    fn on_ack(
+        &mut self,
+        _now: Instant,
+        _sent: Instant,
+        _bytes: u64,
+        _app_limited: bool,
+        _rtt: &RttEstimator,
+    ) {
+    }
+
+    fn on_congestion_event(
+        &mut self,
+        _now: Instant,
+        _sent: Instant,
+        _is_persistent_congestion: bool,
+        _lost_bytes: u64,
+    ) {
+    }
+
+    fn on_mtu_update(&mut self, _new_mtu: u16) {}
+
+    fn window(&self) -> u64 {
+        self.window
+    }
+
+    fn clone_box(&self) -> Box<dyn Controller> {
+        Box::new(self.clone())
+    }
+
+    fn initial_window(&self) -> u64 {
+        self.config.initial_window
+    }
+
+    fn into_any(self: Box<Self>) -> Box<dyn Any> {
+        self
+    }
+}
+
+/// Configuration for the `NoCC` congestion controller
+#[derive(Debug, Clone)]
+pub struct NoCCConfig {
+    initial_window: u64,
+}
+
+impl Default for NoCCConfig {
+    fn default() -> Self {
+        Self {
+            // set to the largest possible value (aka almost infinite)
+            initial_window: u64::MAX,
+        }
+    }
+}
+
+impl ControllerFactory for NoCCConfig {
+    fn build(self: Arc<Self>, now: Instant, current_mtu: u16) -> Box<dyn Controller> {
+        Box::new(NoCC::new(self, now, current_mtu))
+    }
+}
